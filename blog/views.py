ea354574c72e8ee2402b4b0eb448d2a0ad815c6e
Erik Agayan <erik.agayan1@gmail.com>
2024-01-04 19:19:20 +0200

    Add basic post CRUD

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from blog.models import Post
+from blog.serializers import PostListSerializer, PostSerializer
+
+
+class PostViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Post.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PostListSerializer
+        return PostSerializer

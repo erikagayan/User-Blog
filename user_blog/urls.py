b5fe1eccdeaa7e4e41964a4f1f08743ecc21024a
Erik Agayan <erik.agayan1@gmail.com>
2024-01-07 21:21:25 +0200

    Add users

--- user_blog/urls.py
+++ user_blog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/blog/", include("blog.urls", namespace="blog")),
+    path("api/users/", include("users.urls", namespace="users")),
 ]
